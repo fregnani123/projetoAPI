7ed51c266fe8b56a1982549387ce2f3dd519f7c1
fregnani123 <fabianofregnani@hotmail.com>
2023-09-08 19:31:08 -0300

    estudos

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-import express, { response } from "express";
+import express from 'express';
 const app = express()
 import dotEnv from 'dotenv';
 dotEnv.config({ path: './.env' });
 import mongoose from 'mongoose';
-import User from './models/NewUser.js'
+import User from './models/NewUser.js';
+import cors from 'cors'
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT
 const PASSWORD = process.env.PASSWORD
+app.use(cors())
 
 const users = []
 
-app.get('/users',  (req, res) => {
+app.get('/users', (req, res) => {
     return res.json(users)
 })
 
@@ -26,7 +28,7 @@ app.post('/users', async (req, res) => {
     const user = req.body
     const newUser = await User.create(user)
     return res.json(newUser)
-    
+
 })
 
 // app.post('/post', (req, res) => {
