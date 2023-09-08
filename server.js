f9fc9bd7d9fbe1a29b7c902e680a041831ba1cec
fregnani123 <fabianofregnani@hotmail.com>
2023-09-08 18:53:18 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,46 @@
+import express, { response } from "express";
+const app = express()
+import dotEnv from 'dotenv';
+dotEnv.config({ path: './.env' });
+import mongoose from 'mongoose';
+import User from './models/NewUser.js'
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT
+const PASSWORD = process.env.PASSWORD
+
+const users = []
+
+app.get('/users',  (req, res) => {
+    return res.json(users)
+})
+
+app.get('/find', async (req, res) => {
+    const users = await User.find()
+    return res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+    const user = req.body
+    const newUser = await User.create(user)
+    return res.json(newUser)
+    
+})
+
+// app.post('/post', (req, res) => {
+//     const { nome, age, nickName } = req.body
+//     users.push[{ nome, age, nickName }]
+//     return res.json({ nome, age, nickName })
+// })
+
+mongoose.connect(PASSWORD).then(() => {
+    console.log('Banco de dados conectado')
+}).catch(() => {
+    console.log('Erro não conectou')
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na PORT:${PORT}`)
+})
